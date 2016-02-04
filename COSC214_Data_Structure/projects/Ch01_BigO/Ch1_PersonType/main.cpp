9e0409bd46dd74e484c9a1f450c7e5a7a94c5ad1
Frank Xu <frank.w.xu@gmail.com>
2016-02-04 15:15:40 -0500

    solve sync problem

---
+++ COSC214_Data_Structure/projects/Ch01_BigO/Ch1_PersonType/main.cpp
@@ -0,0 +1,20 @@
+
+//************************************************************
+// Author: D.S. Malik
+// 
+// Implemenation file personType
+//************************************************************
+ 
+#include <iostream>
+#include <string>
+#include "personType.h"
+
+using namespace std;
+
+int main(){
+//create an object to describe you.
+//display your information.
+
+}
+
+
